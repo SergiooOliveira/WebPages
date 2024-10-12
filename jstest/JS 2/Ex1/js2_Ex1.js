22659429d73eb04ac30a339cafe55232cb39184c
SergiooOliveira <58986379+SergiooOliveira@users.noreply.github.com>
2024-10-12 19:43:30 +0100

    Exercicio 2

---
+++ jstest/JS 2/Ex1/js2_Ex1.js
@@ -0,0 +1,60 @@
+const player = {
+    createPlayer: function(name, health, strength) {
+        this.name = name,
+        this.health = health,
+        this.strength = strength
+
+        return player
+    },
+        
+    attack: function() {
+        console.log(this.strength)
+    },
+
+    takeDamage: function(damage) {
+        this.health -= damage
+        console.log("Player took ", damage," damage")
+        if (this.health <= 0) {
+            console.log("Player is dead")
+        }
+    },
+
+    playerInfo: function() {
+        console.log("Name: ", player.name)
+        console.log("Health: ", player.health)
+        console.log("Strength: ", player.strength)
+    }
+}
+
+const enemy = {
+    createEnemy: function(enemyType, health, strength) {
+        this.enemyType = enemyType,
+        this.health = health,
+        this.strength = strength
+
+        return enemy
+    },
+
+    attack: function() {
+        console.log(this.strength)
+    },
+
+    dropLoot: function() {
+        this.type = "Gold",
+        this.amount = Math.floor((Math.random() * 50) + 1)
+
+        return this
+    }
+}
+
+const player1 = player.createPlayer("Mark", 100, 20)
+const enemy1 = enemy.createEnemy("Goblin", 50, 15)
+
+
+player1.attack()
+player1.playerInfo()
+player1.takeDamage(30)
+enemy1.attack()
+
+const loot = enemy1.dropLoot()
+console.log("Loot type = ", loot.type,". Loot amount = ", loot.amount)
